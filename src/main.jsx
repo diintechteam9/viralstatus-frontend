4dc81ad50f2296dca2eef78e2d03a5964a83873b
diintechteam9 <diintechteam9@gmail.com>
2025-09-30 13:33:25 +0530

    logo

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
-const GOOGLE_CLIENT_ID = "1059377980980-mug2vtt9up6rpopgm820tt5v47mgvjqm.apps.googleusercontent.com"
+const GOOGLE_CLIENT_ID = "https://1059377980980-10arp8a06d3i2e9ukc5effempl7djcfr.apps.googleusercontent.com"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
